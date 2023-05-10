141676c0b9f29f1375c2c19e1610949b58d67857
LAMINE AOUDJIT <91609151+hpipou@users.noreply.github.com>
2023-05-10 13:17:59 +0200

    OFFLINE SERVER NOTIFICATION MAIL

--- index.js
+++ index.js
@@ -1,12 +1,22 @@
 const fs = require('fs');
 const axios = require('axios');
 const moment = require('moment');
+const nodemailer = require('nodemailer');
 
 // On va tester sur GOOGLE :p
 const serverURL = 'https://www.google.com'; 
 
 // Fichier texte ou seront stocker les logs
 const logFilePath = 'logs.txt'; 
+
+// Configurer le transporteur de messagerie avec Outlook
+const transporter = nodemailer.createTransport({
+  service: 'gmail',
+  auth: {
+    user: '------------@gmail.com', // Adresse e-mail de l'expéditeur
+    pass: '------------', // Mot de passe de l'expéditeur
+  },
+});
 
 async function checkServerHealth() {
   try {
@@ -18,10 +28,12 @@ async function checkServerHealth() {
     } else {
       const logMessage = `[${moment().format('YYYY-MM-DD HH:mm:ss')}] Le serveur a retourné une réponse inattendue.\n`;
       saveLog(logMessage);
+      sendNotificationEmail(logMessage);
     }
   } catch (error) {
     const logMessage = `[${moment().format('YYYY-MM-DD HH:mm:ss')}] OFFLINE.\n`;
     saveLog(logMessage);
+    sendNotificationEmail(logMessage);
   }
 }
 
@@ -29,6 +41,23 @@ function saveLog(logMessage) {
   fs.appendFile(logFilePath, logMessage, (err) => {
     if (err) {
       console.error('Erreur lors de la sauvegarde du log :', err);
+    }
+  });
+}
+
+function sendNotificationEmail(message) {
+  const mailOptions = {
+    from: '-------@gmail.com', // Adresse e-mail de l'expéditeur
+    to: '---------@gmail.com', // Adresse e-mail du destinataire
+    subject: 'Alerte de surveillance de serveur', // Sujet de l'e-mail
+    text: message, // Corps du message de l'e-mail
+  };
+
+  transporter.sendMail(mailOptions, (error, info) => {
+    if (error) {
+      console.error('Erreur lors de l\'envoi de l\'e-mail :', error);
+    } else {
+      console.log('E-mail envoyé avec succès :', info.response);
     }
   });
 }
